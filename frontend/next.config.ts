734a72e2db97046025bfaabc6c35b085d374d3a0
Godwin O <51770832+godwinolle@users.noreply.github.com>
2025-02-08 08:45:36 -0500

    using nextjs image component (#2)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['media.api-sports.io'], // Team logos here
+  },
 };
 
 export default nextConfig;
