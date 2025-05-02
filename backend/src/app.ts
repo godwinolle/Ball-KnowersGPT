077b6696c7a9cc71e819b0542289e4b4cb8fd713
Godwin O <51770832+godwinolle@users.noreply.github.com>
2025-05-02 04:20:34 -0400

    fine tuning llm to use api data now (#7)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,21 @@
 import cors from 'cors'
 import dotenv from 'dotenv'
-import express, { Express } from 'express'
+import express, { Express, Request, Response } from 'express'
 
 import openAIChat from './routes/openAIRoute'
 import playerGame from './routes/playerGameRoute'
 
 dotenv.config()
 const app: Express = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const corsOption = {
     origin: '*'
 }
+
+app.get('/api/health', (_req: Request, res: Response) => {
+    res.send('Server is running!');
+})
 
 // Middleware
 app.use(express.json());
@@ -21,5 +25,5 @@ app.use('/openai/chat', openAIChat)
 app.use('/api/interactive-game', playerGame)
 
 app.listen(PORT, () => {
-    console.log('Server is up and running on port 3000')
+    console.log(`Server is up and running on PORT: ${PORT}`)
 })
---
+++ backend/src/lib/externalApi.ts
@@ -0,0 +1,79 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+let apiKey = process.env.RAPID_API_KEY
+let apiHost = process.env.RAPID_API_HOST
+let apiUrl = process.env.RAPID_API_URL
+
+// async function fetchFootballData(endpoint: string, params) {
+
+// }
+
+async function fetchRecentTeamPerformance() {
+    const url: string = `${apiUrl}/standings?league=39&season=2024`
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey as string,
+            'x-rapidapi-host': apiHost as string
+        }
+    }
+
+    try {
+        const response = await fetch(url, options)
+        const data = await response.json()
+        const standings = await data.response[0].league.standings[0]
+
+        return standings
+    } catch (error) {
+        console.error('Error while retrieving premier league table')
+    }
+}
+
+async function fetchTopScorers() {
+    const url: string = `${apiUrl}/players/topscorers?league=39&season=2024`
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey as string,
+            'x-rapidapi-host': apiHost as string
+        }
+    }
+
+    try {
+        const response = await fetch(url, options)
+        const data = await response.json()
+        const standings = await data.response
+
+        return standings
+    } catch (error) {
+        console.error('Error while retrieving premier league top goalscorers.')
+    }
+}
+
+async function fetchTopAssisters() {
+    const url: string = `${apiUrl}/players/topassisters?league=39&season=2024`
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey as string,
+            'x-rapidapi-host': apiHost as string
+        }
+    }
+
+    try {
+        const response = await fetch(url, options)
+        const data = await response.json()
+        const standings = await data.response[0]
+
+        return standings
+    } catch (error) {
+        console.error('Error while retrieving premier league top assisters.')
+    }
+}
+
+export { fetchRecentTeamPerformance, fetchTopScorers, fetchTopAssisters }
