f4153d7a54a457bbdf0c2b0a368dc5ce9dfb838d
godwinolle <godwinolle@gmail.com>
2025-06-19 23:48:49 -0400

    function calling working

--- backend/src/controllers/openAIController.ts
+++ backend/src/controllers/openAIController.ts
@@ -1,5 +1,5 @@
 import { openai } from "../lib/openAI";
-import { instructions } from "../lib/prompts";
+import { instructions, functionCallingInstructions } from "../lib/prompts";
 
 import { ChatCompletionMessageParam } from "openai/resources/chat/completions";
 
@@ -9,6 +9,7 @@ import knowledgeBase = require('../knowledgeBase/easyPlayers.json')
 
 const availableFunctions = {
     fetchTopScorers: {
+        name: "fetchTopScorers",
         description: "Get the current top goal scorers in the Premier League.",
         parameters: {
             type: "object",
@@ -17,6 +18,7 @@ const availableFunctions = {
         }
     },
     fetchTopAssisters: {
+        name: "fetchTopAssisters",
         description: "Get the current top assisters in the Premier League.",
         parameters: {
             type: "object",
@@ -25,6 +27,7 @@ const availableFunctions = {
         }
     },
     fetchRecentTeamPerformance: {
+        name: "fetchRecentTeamPerformance",
         description: "Get the current Premier League standings and team performance.",
         parameters: {
             type: "object",
@@ -34,90 +37,15 @@ const availableFunctions = {
     }
 }
 
-const isRecentInformationQuery = (userPrompt: string): boolean => {
-    let regex = /(202[4-9]|current|recent|today|now|latest|this (year|season|month|week)|last (month|week|few (days|weeks|months))|past (month|week|few (days|weeks|months))|up to date|up-to-date|updates?|newest|most recent|recently|lately|as of|since)/i
-    return regex.test(userPrompt)
-}
-
-const isPlayerStatsQuery = (userPrompt: string): boolean => {
-    let regex = /(goal(s|scorer)?|assist(s|er)?|player|score[ds]|top scorer|leading scorer|most goals|most assists|golden boot|stat(s|istic))/i
-    return regex.test(userPrompt)
-}
-
-const isTeamPerformanceQuery = (userPrompt: string): boolean => {
-    let regex = /(team|club|standing|position|rank|league table|who (is|are) (winning|leading|at the top)|how (is|are) .+ (doing|performing|playing)|who (will|might|could|is likely to) win)/i
-    return regex.test(userPrompt)
-}
-
-const isGoalScorerQuery = (userPrompt: string): boolean => {
-    const goalTerms = /(goal(s|scorer)?|top scorer|leading scorer|most goals|golden boot|who (has|have) scored|goal tally|goal count|top of the scoring chart)/i;
-    return goalTerms.test(userPrompt);
-}
-
-const isAssistQuery = (userPrompt: string): boolean => {
-    const assistTerms = /(assist(s|er)?|most assists|top assist|playmaker|chance creator|who (has|have) (the most|created) assists|assist tally|assist count)/i;
-    return assistTerms.test(userPrompt);
-}
-
-// const generateChat = async (userPrompt: string): Promise<string> => {
-//     const { isRecent, isPlayerStats, isTeamPerformance, statType, apiData } = await buildPromptData(userPrompt)
-
-//     const messages: ChatCompletionMessageParam[] = [
-//         {
-//             role: "system",
-//             content: instructions
-//         },
-//         {
-//             role: "assistant",
-//             content: instructionsToKnowledgeBase()
-//         }
-//     ];
-
-//     if (apiData) {
-//         let contextMessage = ""
-//         let apiKnowledge = JSON.stringify(apiData)
-
-//         if (isRecent) {
-//             if (isPlayerStats) {
-//                 if (statType === "assists") {
-//                     contextMessage = `Here is the most up-to-date information on the current top assist providers in the Premier League this season: ${apiKnowledge}`;
-//                 } else {
-//                     contextMessage = `Here is the most up-to-date information on the current top scorers in the Premier League this season: ${ apiKnowledge }`
-//                 }
-//             } else if (isTeamPerformance) {
-//                 contextMessage = `Here is the most up-to-date information on the current Premier League standings this season: ${ apiKnowledge }`
-//             }
-
-//             // Add the context message if we have one
-//             if (contextMessage) {
-//                 messages.push( {
-//                     role: "assistant",
-//                     content: contextMessage
-//                 } );
-//             }
-//         }
-//     }
-
-//     messages.push( {
-//         role: "user",
-//         content: userPrompt
-//     } );
-
-//     const completion = await openai.chat.completions.create({
-//         model: "gpt-4o-mini",
-//         messages: messages
-//     })
-
-//     let chatAnswer = completion.choices[0].message.content
-
-//     return chatAnswer as string;
-// }
-
 const generateChat = async (userPrompt: string): Promise<string> => {
     const messages: ChatCompletionMessageParam[] = [
         {
             role: "system",
             content: instructions
+        },
+        {
+            role: "system",
+            content: functionCallingInstructions
         },
         {
             role: "assistant",
@@ -129,6 +57,7 @@ const generateChat = async (userPrompt: string): Promise<string> => {
         }
     ];
 
+    // First call: This will determine if the LLM will use a function or its own knowledge base.
     const firstCompletion = await openai.chat.completions.create({
         model: "gpt-4o-mini",
         messages: messages,
@@ -152,6 +81,8 @@ const generateChat = async (userPrompt: string): Promise<string> => {
     const responseMessage = firstCompletion?.choices[0].message
     messages.push(responseMessage)
 
+    console.log("Response Message", responseMessage.tool_calls)
+
     // If the LLM determines that it needs to call a function, execute that function
     if (responseMessage.tool_calls) {
         for (const toolCall of responseMessage.tool_calls) {
@@ -159,47 +90,47 @@ const generateChat = async (userPrompt: string): Promise<string> => {
             let functionResult;
 
             console.log('This is the function name', functionName)
+
+            try {
+                switch (functionName) {
+                    case 'fetchTopScorers':
+                        functionResult = await fetchTopScorers()
+                        break
+                    
+                    case 'fetchTopAssisters':
+                        functionResult = await fetchTopAssisters()
+                        break
+
+                    case 'fetchRecentTeamPerformance':
+                        functionResult = await fetchRecentTeamPerformance()
+                        break
+
+                    default: 
+                        functionResult = { error: 'Unknown function' }
+                }
+
+            } catch (error) {
+                functionResult = { error: `Error while calling function ${ functionName }: ${ error }` }
+            }
+
+            // This is responsible for adding the function results to the conversation
+            messages.push({
+                role: "tool",
+                tool_call_id: toolCall.id,
+                content: JSON.stringify(functionResult)
+            })
         }
+
+        // Second call: This will let the LLM respond with the function results
+        const secondCompletion = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: messages
+        });
+
+        return secondCompletion.choices[0].message.content as string;
     }
 
     return responseMessage.content as string
-}
-
-const buildPromptData = async (userPrompt: string) => {
-    let isRecent: boolean = isRecentInformationQuery(userPrompt);
-    let isPlayerStats: boolean = isPlayerStatsQuery(userPrompt);
-    let isTeamPerformance: boolean = isTeamPerformanceQuery(userPrompt);
-
-    let isGoalScorer: boolean = isGoalScorerQuery(userPrompt);
-    let isAssist: boolean = isAssistQuery(userPrompt);
-
-    let apiData = null;
-    let statType: 'assists' | 'goals' | '' = '';
-
-    try {
-        if (isRecent) {
-            if (isPlayerStats) {
-                if (isAssist) {
-                    apiData = await fetchTopAssisters()
-                    statType = 'assists'
-                } else if (isGoalScorer) {
-                    apiData = await fetchTopScorers()
-                    statType = 'goals'
-                } else {
-                    apiData = await fetchTopScorers();
-    
-                }
-            } else if (isTeamPerformance) {
-                apiData = await fetchRecentTeamPerformance()
-            } 
-        }
-
-        console.log("This is the data that is returned", apiData)
-    } catch (error) {
-        console.error('Error while trying to retrieve API data for LLM knowledge base')
-    }
-
-    return { isRecent, isPlayerStats, isTeamPerformance, isGoalScorer, isAssist, statType, apiData }
 }
 
 const instructionsToKnowledgeBase = () => {
