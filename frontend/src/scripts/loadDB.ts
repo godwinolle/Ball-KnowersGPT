99be7b1c0628ffdc0ebc405bc31ff16b833d6e1d
godwinolle <godwinolle@gmail.com>
2026-08-10 21:03:45 -0400

    initial commit

---
+++ frontend/src/scripts/loadDB.ts
@@ -0,0 +1,14 @@
+import { openai } from "@/lib/openAI";
+
+
+const wikiPages: string[] = [
+    "https://en.wikipedia.org/wiki/2024%E2%80%9325_Premier_League",
+    "https://en.wikipedia.org/wiki/2025%E2%80%9326_Premier_League",
+    "https://en.wikipedia.org/wiki/2026%E2%80%9327_Premier_League",
+    "https://en.wikipedia.org/wiki/Liverpool_F.C.",
+    "https://en.wikipedia.org/wiki/Manchester_City_F.C.",
+    "https://en.wikipedia.org/wiki/Manchester_United_F.C.",
+    "https://en.wikipedia.org/wiki/Chelsea_F.C.",
+    "https://en.wikipedia.org/wiki/Arsenal_F.C.",
+    "https://en.wikipedia.org/wiki/Tottenham_Hotspur_F.C.",
+];
